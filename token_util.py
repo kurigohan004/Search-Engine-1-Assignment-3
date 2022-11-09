7a34ff90be14e9be3ba06108ca3418cdeb04bb39
kurigohan004 <hsaul0201@gmail.com>
2022-11-09 13:23:04 -0800

    Create file for tokenizer functions, changed tf for postings, created index for index function
    new index function helps make file lookups in index faster as we save term positions, tokenizer functions being stored in a separate file will help once we implement queries, and our term frequency for our postings was incorrect so we had to fix it.

---
+++ token_util.py
@@ -0,0 +1,21 @@
+import re
+
+def tokenize(text):
+    text = text.lower()
+    l = 0
+    while l < len(text) and not ("a" <= text[l] <= "z" or "0" <= text[l] <= "9"):
+        l += 1
+    r = len(text)-1
+    while r >= 0 and not ("a" <= text[r] <= "z" or "0" <= text[r] <= "9"):
+        r -= 1
+    tokens = re.split(r"[^a-z0-9]+", text[l:r+1])
+    return [token for token in tokens if token != ""]
+
+def compute_token_frequencies(tokens):
+    token_frequencies = {}
+    for token in tokens:
+        if token not in token_frequencies:
+            token_frequencies[token] = 1
+        else:
+            token_frequencies[token] += 1
+    return token_frequencies
